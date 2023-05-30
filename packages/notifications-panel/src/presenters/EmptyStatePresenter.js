507b355f81420bd2fae6c991a027eb35a08c1b00
Enzo Batista <111511512+Enzo707@users.noreply.github.com>
2023-05-30 10:53:00 -0300

    feat:update EmptyStatePresenter and share any notification dismiss event back to NotificationCenter (#3)

--- packages/notifications-panel/src/presenters/EmptyStatePresenter.js
+++ packages/notifications-panel/src/presenters/EmptyStatePresenter.js
@@ -7,23 +7,30 @@ import Bell from "./Bell.svg";
 import stylesheet from "./stylesheet";
 
 export default function EmptyStatePresenter({
-  message,
-  stylesheet: customStylesheet,
+	title,
+	message,
+	image,
+	stylesheet: customStylesheet,
 }) {
-  const styles = stylesheet({ stylesheet: customStylesheet }, {});
-  return (
-    <div className={css(styles.emptyState)}>
-      <Bell role="presentation" className={css(styles.emptyStateImage)} />
-      <Typography style={styles.emptyStateMessage}>{message}</Typography>
-    </div>
-  );
+	const styles = stylesheet({ stylesheet: customStylesheet }, {});
+	const defaultImage = (
+		<Bell role="presentation" className={css(styles.emptyStateImage)} />
+	);
+	return (
+		<div className={css(styles.emptyState)}>
+			{image ? image : defaultImage}
+			<Typography variant={"h3"} style={styles.emptyStateTitle}>{title}</Typography>
+			<Typography style={styles.emptyStateMessage}>{message}</Typography>
+		</div>
+	);
 }
 
 EmptyStatePresenter.defaultProps = {
-  message: "You currently have no notifications",
+	message: "You currently have no notifications",
 };
 
 EmptyStatePresenter.propTypes = {
-  message: PropTypes.string,
-  stylesheet: PropTypes.func,
+  title: PropTypes.string,
+	message: PropTypes.string,
+	stylesheet: PropTypes.func,
 };
